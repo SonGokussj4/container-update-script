0ce8b08f98d5bd76fafe720aedb9d9698c05d31f
Jan Verner <SonGokussj4@centrum.cz>
2025-02-25 16:17:19 +0100

    Refactor command checking functions and improve help message formatting

--- cups.sh
+++ cups.sh
@@ -7,53 +7,46 @@ SPECIFIC_SERVICES=""
 # File to store the output of 'cup check' command
 INPUT_FILE="/tmp/results.txt"
 
-check_for_command_cup() {
-    if ! command -v cup &> /dev/null; then
-        echo "Error: 'cup' command not found. Please install it from https://sergi0g.github.io/cup/docs/installation/binary"
+# Function to check if a command exists
+check_for_command() {
+    local cmd=$1
+    local url=$2
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "Error: '$cmd' command not found. Please install it from $url"
         exit 1
     fi
 }
-
-
-check_for_command_jq() {
-    if ! command -v jq &> /dev/null; then
-        echo "Error: 'jq' command not found. Please install it from https://jqlang.github.io/jq/download/"
-        exit 1
-    fi
-}
-
 
 # Function to display help
 show_help() {
-    echo "Usage: update_containers [options]"
-    echo
-    echo "Options:"
-    echo "  --tags <tags>       Specify tags to filter (comma-separated, e.g., 'latest,release,stable')."
-    echo "  --services <names>  Specify services to update (comma-separated, e.g., 'deluge,radarr')."
-    echo "  --check             Check for available updates without performing any actions."
-    echo "  --help              Display this help message."
+    cat << EOF
+Usage: update_containers [options]
+
+Options:
+  --tags <tags>       Specify tags to filter (comma-separated, e.g., 'latest,release,stable').
+  --services <names>  Specify services to update (comma-separated, e.g., 'deluge,radarr').
+  --check             Check for available updates without performing any actions.
+  --help              Display this help message.
+EOF
     exit 0
 }
-
 
 # Parse arguments
 TAGS="$DEFAULT_TAGS"
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
         --tags)
-            # TAGS=$(echo "$2" | sed 's/,/|/g')
             TAGS="${2//,/|}"
             shift 2
             ;;
         --services)
-            # SPECIFIC_SERVICES=$(echo "$2" | sed 's/,/ /g')
             SPECIFIC_SERVICES=${2//,/ }
             shift 2
             ;;
         --check)
-            check_for_command_cup
-            cup check > $INPUT_FILE
-            grep 'Update available' $INPUT_FILE
+            check_for_command "cup" "https://sergi0g.github.io/cup/docs/installation/binary"
+            cup check > "$INPUT_FILE"
+            grep 'Update available' "$INPUT_FILE"
             exit 0
             ;;
         --help)
@@ -67,23 +60,22 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Check for required commands
-check_for_command_cup
-check_for_command_jq
+check_for_command "cup" "https://sergi0g.github.io/cup/docs/installation/binary"
+check_for_command "jq" "https://jqlang.github.io/jq/download/"
 
 # Run the 'cup check' command and save the output to a file for further processing
-cup check > $INPUT_FILE
+cup check > "$INPUT_FILE"
 
 # Filter lines that match the specified criteria
-mapfile -t update_lines < <(grep 'Update available' $INPUT_FILE | grep -E "$TAGS")
+mapfile -t update_lines < <(grep 'Update available' "$INPUT_FILE" | grep -E "$TAGS")
 
 # Get a list of all Docker Compose projects and their directories
 mapfile -t compose_projects < <(docker compose ls --format json | jq -r '.[] | "\(.Name) \(.ConfigFiles)"')
 
-# Just a flag to indicate if any updates were performed
+# Flag to indicate if any updates were performed
 updated_any=false
 
 # Iterate over the list of Docker Compose projects
-# Loop through each line that indicates an update is available
 for line in "${update_lines[@]}"; do
     # Extract the image name (e.g., apache/tika:latest)
     image=$(echo "$line" | awk '{print $1}')
@@ -101,7 +93,6 @@ for line in "${update_lines[@]}"; do
 
     # Find the corresponding Docker Compose project
     for project in "${compose_projects[@]}"; do
-
         project_name=$(echo "$project" | awk '{print $1}')
         compose_file=$(echo "$project" | awk '{print $2}')
         project_dir=$(dirname "$compose_file")
@@ -109,7 +100,6 @@ for line in "${update_lines[@]}"; do
         # Check if the service is part of this project
         if grep -q "$service_name" "$compose_file"; then
             echo "PROJECT: $project"
-
             echo "Updating service '$service_name' in project '$project_name'..."
 
             # Navigate to the project directory
@@ -127,14 +117,18 @@ for line in "${update_lines[@]}"; do
             updated_any=true
         fi
     done
-done
 
-if ! $updated_any; then
-    echo "No updates were performed."
-    if grep -q 'Update available' "$INPUT_FILE"; then
-        echo "Here's the list of all 'Update available' items:"
-        grep 'Update available' "$INPUT_FILE"
-    else
-        echo "And no updates available at the moment."
+    echo "------------------------------------------------------------------------------------"
+
+    # Check if any updates were performed
+    if ! $updated_any; then
+        echo "No updates were performed."
+        if grep -q 'Update available' "$INPUT_FILE"; then
+            echo "Here's the list of all 'Update available' items:"
+            grep 'Update available' "$INPUT_FILE"
+        else
+            echo "And no updates available at the moment."
+        fi
     fi
-fi
+
+done
